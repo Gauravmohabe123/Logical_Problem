acb127d6a5f50f7d859cbe9ea2ca11c9ed3a8efd
Gaurav Mohabe <gmohabe12@gmail.com>
2023-01-30 10:59:31 +0530

    Added Assignment problem

---
+++ Reverse_num.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Transactions;
+
+namespace Reverse_num
+{
+    internal class Reverse_num
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            Console.WriteLine("Enter the to reverse numbur");
+            int num = int.Parse(Console.ReadLine());
+            int reverse = 0;
+            while (num>0)
+            {
+                int remainder= num % 10;
+                reverse = (reverse * 10) + remainder;
+
+                num = num / 10;
+            }
+            Console.WriteLine("Reverse numbur is: " +reverse);
+
+            
+        }
+    }
+}
