87f65f5cbb409535f139345d8e5cadbf983a0485
Gaurav Mohabe <gmohabe12@gmail.com>
2023-01-30 15:54:28 +0530

    Added Assignment problem

---
+++ Stopwatch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+
+namespace StopWatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch=new Stopwatch();    
+            stopwatch.Start();
+            for(int i=0;i<10;i++)
+            {
+                Console.WriteLine("Tike");
+            }
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Elapsed);
+
+        }
+    }
+}
